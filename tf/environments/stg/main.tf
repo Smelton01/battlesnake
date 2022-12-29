4f09a032ef55335f6f67e91a57be820ec43252c7
Simon Juba <scimail09@gmail.com>
2022-12-29 17:19:48 +0900

    add github oidc provider

--- tf/environments/stg/main.tf
+++ tf/environments/stg/main.tf
@@ -96,3 +96,14 @@ resource "aws_lambda_permission" "api_gw" {
 
   source_arn = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
 }
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+  
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+  ]
+}
